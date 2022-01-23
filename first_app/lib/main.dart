87bf03e077635cfa49d3cdcec0866f6d124d9ff7
Krish Gupta <guptakrish40@gmail.com>
2022-01-23 18:00:18 +0530

    Basic theme and learn about text styles updated

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first_app/home_page.dart';
+import 'package:first_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
     );
   }
 }
