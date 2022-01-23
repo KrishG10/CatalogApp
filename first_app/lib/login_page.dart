09f11d31f2c285449f7e66b642c0b46aab4d8cd6
Krish Gupta <guptakrish40@gmail.com>
2022-01-23 22:58:02 +0530

    Add image and learn basics about textformfield, padding, sizedbox and elevated button

--- first_app/lib/login_page.dart
+++ first_app/lib/login_page.dart
@@ -4,8 +4,21 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login page"),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/Image/login.png"),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
       ),
     );
   }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/home_page.dart';
 import 'package:first_app/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+// initialRoute: "/home",
+
       routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
     );
   }
