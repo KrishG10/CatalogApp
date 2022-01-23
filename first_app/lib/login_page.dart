d522ca142715980167511954898cc1c86fb2855b
Krish Gupta <guptakrish40@gmail.com>
2022-01-23 22:58:29 +0530

    Add image and learn basics about textformfield, padding, sizedbox and elevated button updated

--- first_app/lib/login_page.dart
+++ first_app/lib/login_page.dart
@@ -17,6 +17,38 @@ class LoginPage extends StatelessWidget {
               fontSize: 24,
               fontWeight: FontWeight.bold,
             ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                    // ignore: prefer_const_constructors
+                    decoration: InputDecoration(
+                  hintText: " Enter your Username",
+                  labelText: "Username",
+                )),
+                TextFormField(
+                    obscureText: true,
+                    // ignore: prefer_const_constructors
+                    decoration: InputDecoration(
+                      hintText: " Enter Password",
+                      labelText: "Password",
+                    )),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  onPressed: () {},
+                  style: TextButton.styleFrom(),
+                )
+              ],
+            ),
           )
         ],
       ),
